23cdd2c7636092369c6ddfb7f65245c9fb696ca7
AMOO <amoosamson@gmail.com>
2023-07-31 20:24:37 +0100

    school management system

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.all_category, name='all_category'),
     path('category_question/<int:cat_id>/', views.category_question, name='category_question'),
     path('submit-answer/<int:cat_id>/<int:quest_id>', views.submit_answer, name='submit_answer'),
+    path('attempt-limit', views.attempt_limit, name='attempt-limit'),
+    path('result', views.result, name='result'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from datetime import timedelta
 # Create your views here.
 from . import models
 
@@ -18,9 +18,36 @@ def all_category(request):
 def category_question(request, cat_id):
     category = models.QuizCategory.objects.get(id=cat_id)
     question=models.QuizQuestion.objects.filter(category=category).order_by('id').first()
+
+    # check current user last attempt
+    lastAttempt = None
+    futureTime = None
+    hoursLimit = 24
+    countAttempt=models.UserCategoryAttempts.objects.filter(user=request.user, category=category).count()
+    # if no attempt then insert row 
+
+    if countAttempt == 0:
+         models.UserCategoryAttempts.objects.create(user=request.user, category=category)
+        #  else check last attempt time 
+    else:
+        lastAttempt=models.UserCategoryAttempts.objects.filter(user=request.user, category=category).order_by('id').first()
+        futureTime = lastAttempt.attempt_time + timedelta(hours=hoursLimit) 
+        # if last future < lastAttempt, show warnning message 
+
+        if lastAttempt and lastAttempt.attempt_time < futureTime:
+            return redirect('attempt-limit')
+            
+        
+        else:
+            #  insert another attempt 
+            models.UserCategoryAttempts.objects.create(user=request.user, category=category)
+
+            
+        
     context={
         'question':question,
-        'category': category
+        'category': category,
+        'lastAttempt': futureTime
     }
     return render(request, 'quizs/category_question.html', context)
 
@@ -41,7 +68,7 @@ def submit_answer(request, cat_id, quest_id):
                     'category': category
                 }
                 return render(request, 'quizs/category_question.html', context ) 
-            else:
+        else:
                 quest=models.QuizQuestion.objects.get(id=quest_id)
                 user=request.user
                 answer=request.POST['answer'] 
@@ -53,73 +80,65 @@ def submit_answer(request, cat_id, quest_id):
         # question=models.QuizQuestion.objects.filter(category=category, id__gt=quest_id).exclude(id=quest_id).order_by('id').first()
 
         if question:
-             
+                
             context={
-                'question':question,
-                'category': category
-            }
+                    'question':question,
+                    'category': category
+                }
             return render(request, 'quizs/category_question.html', context)
         else:
-            return HttpResponse('No more question!')
+            result = models.UserSubmittedAnswer.objects.filter(user=request.user)
+            skipped = models.UserSubmittedAnswer.objects.filter(user=request.user,  right_answer= "not submitted" ).count()
+            attempted  = models.UserSubmittedAnswer.objects.filter(user=request.user).exclude(right_answer= "not submitted").count()
+            rightAns=0
+            percentage= 0
+            for row in result:
+                 if row.question.right_opt  == row.right_answer:
+                    rightAns +=1
+            percentage= (rightAns*100)/result.count()
+
+            context={
+                    'result':result,
+                    'total_skipped': skipped,
+                    'attempted': attempted,
+                    'rightAns': rightAns,
+                    'percentage': percentage
+                    
+                }
+
+            return render(request, 'quizs/result.html', context)
 
     else:
          return HttpResponse('method not allowed!')
+    
 
 
 
+def attempt_limit(request):
+   
+    return render(request, 'quizs/attempt-limit.html')
 
-# @login_required(login_url=('login'))
-# def submit_answer(request, cat_id, quest_id):
-#     if request.method =='POST': 
-#         category = models.QuizCategory.objects.get(id=cat_id)
-#         question=models.QuizQuestion.objects.filter(category=category, id__gt=quest_id).exclude(id=quest_id).order_by('id').first()
-        
-#         if 'skip' in request.POST:
-#             if question:
-#                 quest= models.QuizQuestion.objects.get(id=quest_id)
-#                 user= request.user
-#                 answer = 'Not Submitted'
-#                 models.UserSubmittedAnswer.objects.create(user=user, question=quest, right_answer=answer)
-#                 context={
-#                     'question':question,
-#                     'category': category
-#                 }
-#                 return render(request, 'quizs/category_question.html', context)
-#         else:
-#             quest=models.QuizQuestion.objects.get(id=quest_id)
-#             user = request.user
-#             answer= request.POST['answer']
-#             models.UserSubmittedAnswer.objects.create(user=user, question=quest, right_answer=answer)
-#         if question:
-#             context={
-#                 'question':question,
-#                 'category': category
-#             }
-#             return render(request, 'quizs/category_question.html', context)
-#         else:
-#             result = models.UserSubmittedAnswer.objects.filter(user=request.user)
-#             skipped = models.UserSubmittedAnswer.objects.filter(user=request.user,  right_answer= "not submitted" ).count()
-#             attempted  = models.UserSubmittedAnswer.objects.filter(user=request.user).exclude(right_answer= "not submitted").count()
-            
-#             rightAns=0
-#             percentage= 0
-#             for row in result:
-#                 if row.question.right_opt  == row.right_answer:
-#                     rightAns +=1
-#             percentage= (rightAns*100)/result.count()       
 
-#             context={
-#                 'result':result,
-#                 'total_skipped':skipped,
-#                 'attempted':attempted,
-#                 'rightAns': rightAns,
-#                 'percentage':percentage
-
-#             }
-#             return render(request, 'quizs/result.html', context)
-
-#     else:
-#         return HttpResponse('method not allowed!')
+def result(request):
+     result = models.UserSubmittedAnswer.objects.filter(user=request.user)
+     skipped = models.UserSubmittedAnswer.objects.filter(user=request.user,  right_answer= "not submitted" ).count()
+     attempted  = models.UserSubmittedAnswer.objects.filter(user=request.user).exclude(right_answer= "not submitted").count()
+     rightAns=0
+     percentage= 0
+     for row in result:
+         if row.question.right_opt  == row.right_answer:
+            rightAns +=1
+     percentage= (rightAns*100)/result.count()
+     context={
+                    'result':result,
+                    'total_skipped': skipped,
+                    'attempted': attempted,
+                    'rightAns': rightAns,
+                    'percentage': percentage
+                    
+                }
+   
+     return render(request, 'quizs/result.html', context)
     
 
 @login_required(login_url=('login'))
